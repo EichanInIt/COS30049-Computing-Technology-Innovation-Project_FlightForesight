b3134b7c55bc598961890fe61bede379e28c6945
ruchanswin <justinnguyen3159@gmail.com>
2024-10-18 03:41:09 +1100

    h

--- flightforesight/src/App.jsx
+++ flightforesight/src/App.jsx
@@ -1,11 +1,13 @@
 // src/App.jsx
 import './App.css';
 import CustomNavbar from './components/Navbar';
+import FlightSearch from './components/InputForm';
 
 function App() {
   return (
     <>
       <CustomNavbar />
+      <FlightSearch />
     </>
   );
 }
