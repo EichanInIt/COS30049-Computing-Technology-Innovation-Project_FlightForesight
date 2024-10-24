6a26a53df24ed1ca73b8bc833a470a9624d4e51a
ruchanswin <justinnguyen3159@gmail.com>
2024-10-25 04:24:27 +1100

    h

--- flightforesight/src/App.jsx
+++ flightforesight/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import CustomNavbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/home'; 
